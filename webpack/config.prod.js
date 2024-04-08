3c173296cd0c270174d39009f18316e6364690bf
김하연 <hayeonkim226@gmail.com>
2024-04-08 15:52:47 +0900

    fix: edit webpack prod config

--- webpack/config.prod.js
+++ webpack/config.prod.js
@@ -5,10 +5,6 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require("webpack");
 
-const line = "---------------------------------------------------------";
-const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-process.stdout.write(`${line}\n${msg}\n${line}\n`);
-
 module.exports = {
   mode: "production",
   entry: "./src/main.js",
